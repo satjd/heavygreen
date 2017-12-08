b5130f8320f6e32a83f41640f4fbbfa13e891076
satjd1996 <824909419@qq.com>
2017-12-08 14:58:24 +0800

    2

--- green.py
+++ green.py
@@ -33,7 +33,7 @@ def main(argv):
 			subprocess.call("echo '" + curdate + str(randint(0, 1000000)) +"' > realwork.txt; git add realwork.txt; GIT_AUTHOR_DATE='" + curdate + "' GIT_COMMITTER_DATE='" + curdate + "' git commit -m 'update'; git push;", shell=True)
 			sleep(.5)
 		i += 1
-	subprocess.call("git rm realwork.txt; git commit -m 'delete'; git push;", shell=True)
+	#subprocess.call("git rm realwork.txt; git commit -m 'delete'; git push;", shell=True)
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
